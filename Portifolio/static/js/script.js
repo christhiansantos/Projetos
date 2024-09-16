8eff37fff9f5c17a3487ee00a8193e1e742af9ba
christhiansantos <108822361+christhiansantos@users.noreply.github.com>
2024-09-16 11:45:07 -0300

    Organização o projeto

--- Portifolio/static/js/script.js
+++ Portifolio/static/js/script.js
@@ -2,9 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "img/menu-hamburger-nav-svgrepo-com.svg";
+        document.querySelector('.icon').src = "../static/img/menu-hamburger-nav-svgrepo-com.svg";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "img/close-circle-svgrepo-com.svg";
+        document.querySelector('.icon').src = "../static/img/close-circle-svgrepo-com.svg";
     }
 }
