d2339ddedc7f92deb6fde7422e171695a0e5c913
christhiansantos <108822361+christhiansantos@users.noreply.github.com>
2024-09-18 11:48:39 -0300

    adicionei novo projeto, conectei formulario com site para visualizar as mensagens, atualização no botão do formulario

--- Portifolio/static/js/script.js
+++ Portifolio/static/js/script.js
@@ -8,3 +8,25 @@ function menuShow() {
         document.querySelector('.icon').src = "../static/img/close-circle-svgrepo-com.svg";
     }
 }
+
+const button = document.querySelector('.send'); 
+
+const addLoading = () => {
+    button.innerHTML = '<img src="img/spin.avif" class="loading">';
+};
+
+const removeLoading = () => {
+    button.innerHTML = 'Enviar'; 
+};
+
+button.addEventListener('click', function(event) {
+   event.preventDefault(); 
+
+    addLoading();
+
+    setTimeout(function() {
+        removeLoading();
+
+        document.querySelector('form').submit();
+    }, 1000);
+});
